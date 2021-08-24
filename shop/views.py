f2e395745d3d01ddecb0819c14c0b04c7cc7b21f
AnuragAgarwal00 <anurag.aarav143@gmail.com>
2021-08-24 14:58:55 +0530

    Added views & templates, urls for product deatil & product list page.

---
+++ shop/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category)
+    return render(request,
+                    'shop/products/list.html',
+                    {'category': category, 
+                    'categories': categories,
+                    'products': products})
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, slug=slug, pk=id, available=True)
+
+    return render(request, 'shop/products/detail.html', {'product': product})
+
+
