45136a6898a04e5c1a5d7d4a19d2b300d820b427
AnuragAgarwal00 <anurag.aarav143@gmail.com>
2021-08-24 20:00:04 +0530

    Added required field to admin.py

---
+++ orders/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.db import models
+
+from .models import Order, OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'first_name',
+        'last_name',
+        'email',
+        'address',
+        'postal_code',
+        'city',
+        'created',
+        'updated',
+        'paid',
+        ]
+    list_filter = ['created', 'updated', 'paid']
+    inlines = [OrderItemInline]
+
