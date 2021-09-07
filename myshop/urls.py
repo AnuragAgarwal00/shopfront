cf2fd7e06ddc2c7b5ce4b6e7fe76cda8989b6923
AnuragAgarwal00 <anurag.aarav143@gmail.com>
2021-09-07 18:09:31 +0530

    Adeed braintree integration on checkout page.

--- myshop/urls.py
+++ myshop/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include(('cart.urls', 'cart'), namespace='cart')),
     path('orders/', include(('orders.urls', 'orders'), namespace='orders')),
+    path('payment/', include(('payment.urls', 'payment'), namespace='payment')),
     path('', include(('shop.urls', 'shop'), namespace='shop')),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
