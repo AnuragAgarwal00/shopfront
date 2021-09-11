1df61d0a644579d1905376fdbe73599416dda23f
AnuragAgarwal00 <anurag.aarav143@gmail.com>
2021-09-11 12:48:24 +0530

    Added invoice for orders in admin

--- orders/urls.py
+++ orders/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('create/', views.order_create, name='order_create'),
+    path('admin/order/<int:order_id>/', views.order_admin_detail, name='order_admin_detail'),
+    path('admin/order/<int:order_id>/pdf/', views.admin_order_pdf, name='admin_order_pdf'),
+    
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
 
 from .models import Order, OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from orders.tasks import order_created
+import weasyprint
 
 def order_create(request):
     cart = Cart(request)
@@ -32,6 +38,21 @@ def order_create(request):
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'form': form, 'cart': cart})
-    
-    
 
+@staff_member_required
+def order_admin_detail(request, order_id):
+    order = get_object_or_404(Order, pk=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, pk=order_id)
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f"filename={order.id}_order.pdf"
+
+    weasyprint.HTML(string=html).write_pdf(response,
+        stylesheets=[weasyprint.CSS(
+            settings.STATIC_ROOT + 'css/pdf.css'
+        )])
+    return response
